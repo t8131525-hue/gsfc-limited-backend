6f4169a54919804d9e1b6d556abe89f413c56147
ivedantmistry <ivedantmistry@gmail.com>
2025-09-26 20:18:39 +0530

    working commit

--- backend/product_testing_system/pagination.py
+++ backend/product_testing_system/pagination.py
@@ -8,9 +8,9 @@ class StandardResultsSetPagination(PageNumberPagination):
     A standard pagination class that can be reused across the application.
     It uses 'page' for the page number and 'page_size' for the page size query params.
     """
-    page_size = 5  # The default number of items per page
-    page_size_query_param = 'page_size'  # Allows client to override page size e.g. ?page_size=50
-    max_page_size = 1000  # The maximum page size the client can request
+    page_size = 10
+    page_size_query_param = 'page_size'
+    max_page_size = 100
 
     def get_paginated_response(self, data):
         """
